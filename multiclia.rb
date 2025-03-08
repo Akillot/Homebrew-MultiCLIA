0d3bf6ce52b7b19fecaf65ba8b648b3995773acc
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-08 01:19:10 +0100

    Formula has been updated

--- multiclia.rb
+++ multiclia.rb
@@ -1,7 +1,7 @@
 class Multiclia < Formula
   desc "Versatile CLI tool to enhance the terminal experience"
   homepage "https://github.com/Akillot/MultiCLIA"
-  url "https://github.com/Akillot/MultiCLIA/releases/download/v1.1.1/MultiCLIA-1.0-jar-with-dependencies.jar"
+  url "https://github.com/Akillot/MultiCLIA/releases/download/v1.1/MultiCLIA-1.0-jar-with-dependencies.jar"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
