2322803fa2e6680763d51bb2226e355db8eb42db
Nick Zozulia <nickzozulia@gmail.com>
2025-03-23 20:31:49 +0100

    Update multiclia.rb

--- multiclia.rb
+++ multiclia.rb
@@ -1,15 +1,15 @@
 class Multiclia < Formula
   desc "Versatile CLI tool to enhance the terminal experience"
   homepage "https://github.com/Akillot/MultiCLIA"
-  url "https://github.com/Akillot/MultiCLIA/releases/download/java/MultiCLIA-1.0-jar-with-dependencies.jar"
-  sha256 "dcc898549202d78faaa7189cbd7ff0ccf598d56688778af4285db8ebc01fdc95"
+  url "https://github.com/Akillot/MultiCLIA/releases/download/java/MultiCLIA-1.2-jar-with-dependencies.jar"
+  sha256 "819ec0a6710fcda441d0a870488f373565b8d99296cff6f8e73efec7f363ed41"
   license "MIT"
 
   depends_on "openjdk"
 
   def install
-    libexec.install "MultiCLIA-1.0-jar-with-dependencies.jar"
-    bin.write_jar_script libexec/"MultiCLIA-1.0-jar-with-dependencies.jar", "multiclia"
+    libexec.install "MultiCLIA-1.2-jar-with-dependencies.jar"
+    bin.write_jar_script libexec/"MultiCLIA-1.2-jar-with-dependencies.jar", "multiclia"
   end
 
   test do
